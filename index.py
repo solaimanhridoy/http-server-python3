93996ebd3faaa04b3762e7fa0964ea4eee2a6824
lib10 <solaimanhridoy@gmail.com>
2021-01-06 22:44:16 +0600

    Serving static files

--- index.py
+++ index.py
@@ -5,9 +5,14 @@ class basicRequestHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world!")
 
+class staticRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
 if __name__ == "__main__":
     app = tornado.web.Application([
-        (r"/", basicRequestHandler)
+        (r"/", basicRequestHandler),
+        (r"/blog", staticRequestHandler)
     ])
 
     app.listen(8080)
